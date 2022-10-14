638f4de0a3ed78c74675fc2ae04a677b34836d88
Ricardo Martins <ricardo@ricardomartins.net.br>
2022-10-14 16:10:22 +1000

    Nextjs com sass. Exemplo 5. Aula 25060.

---
+++ pages/exemplos/exemplo5.jsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/exemplo5.module.scss'
+export default function exemplo5() {
+    return (
+        <div className={styles.container}>
+            <h1>SASS</h1>
+            <p>Usando o componente <a href="https://sass-lang.com/guide">sass</a>.</p> 
+        </div>
+    )
+}
