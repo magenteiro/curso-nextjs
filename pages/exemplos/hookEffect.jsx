26d38de573ee5b2e7998e027097a217dce7df6d0
Ricardo Martins <ricardo@ricardomartins.net.br>
2022-10-18 14:50:30 +1000

    Intro ao useEffect e correção no Home.module.css

---
+++ pages/exemplos/hookEffect.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+
+export default function hookEffect() {
+    const [tamanho, setTamanho] = useState(0)
+
+    useEffect(()=>{
+        console.log('renderizou ou mudou valor')
+        return () => {console.log('destriui')}
+    }, [tamanho])
+    
+    return (
+        <>
+            <h1>Camisa da Seleção</h1>
+
+            <img src="/images/camisa-selecao.jpg" height="200" />
+
+            <p>Selecione um tamanho</p>
+
+            <button onClick={() => setTamanho('P')}>P</button>
+            <button onClick={() => setTamanho('M')}>M</button>
+            <button onClick={() => setTamanho('G')}>G</button>
+            {tamanho ?
+                <p>Tamanho selecionado: {tamanho}</p> : null
+            }
+        </>
+    )
+}
