117705a07d9238d7bca77325096e49f9ba0fd51e
Ricardo Martins <ricardo@ricardomartins.net.br>
2023-01-12 08:43:10 +1000

    Initial commit from Create Next App

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar"
+import HeroHome from "../components/HeroHome"
+import ProductList from "../components/ProductList"
+import Footer from "../components/Footer"
+
+export default function Index(){
+  return (
+    <>
+      <Navbar/>
+      <HeroHome/>
+      <ProductList/>
+      <Footer/>
+    </>
+  )
+}
